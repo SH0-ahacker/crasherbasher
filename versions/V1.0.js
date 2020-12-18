800ccb880e978bd645bca6f57c4acba2184dfa49
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2020-12-18 20:58:57 -0300

    Update V1.0.js
    THE START
    ⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛⎛

--- versions/V1.0.js
+++ versions/V1.0.js
@@ -1,1 +1,13 @@
-
+function loadCrashebasher() {
+	function crash_stress01(){
+		while (true) {
+			document.location.reload(true)
+		}
+	}
+	
+	function crash_stress02(){
+		while (true) {
+			console.error("⎝⓿⎝⓿⎝⓿☯CRASHIN WITH SUCCES☯⓿⎠⓿⎠⓿⎠")
+		}
+	}
+}  
